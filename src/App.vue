00d0e5f05c98847995f30a4e483dccb39acd5b45
Claudiu C. Bogdan <bogdan.claudiu93@gmail.com>
2019-07-09 20:21:55 +0300

    Control vertical cube position.

--- src/App.vue
+++ src/App.vue
@@ -27,17 +27,18 @@
       <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-cubes)" />
     </svg>
     <svg
-      :style="`position:absolute; left:0; top: ${125*cubeSizeFactor};`"
-      x="0"
-      :y="125*cubeSizeFactor"
+      :style="`position:absolute; left:0; top: ${87.5 *cubeSizeFactor + 1.6*125*cubeSizeFactor * cubePositionY};`"
       patternUnits="userSpaceOnUse"
       :width="125*cubeSizeFactor"
       :height="200*cubeSizeFactor"
       viewBox="0 0 10 16"
     >
-      <path class="left-shade-unit" d="M0 0l5 3v5l-5 -3z" />
-      <path class="right-shade-unit" d="M10 0l-5 3v5l5 -3" />
-      <path class="left-shade-unit" d="M0 0l5 3v5l-5 -3z" />
+      <path class="up-shade-unit" d="M 2.8e-7,3.0051119 5,0.00511184 10,3.0051119 5,6.0" />
+      <path class="right-shade-unit" d="m 10,3.0051119 -5,3 V 11.005112 L 10,8.0051119" />
+      <path
+        class="left-shade-unit"
+        d="m 2.78e-7,3.0051119 5.00000002000001,3 V 11.005112 L 2.78e-7,8.0051119 Z"
+      />
     </svg>
   </div>
 </template>
@@ -48,7 +49,8 @@ export default {
   name: "app",
   data: () => {
     return {
-      cubeSizeFactor: window.innerWidth / 1920
+      cubeSizeFactor: window.innerWidth / 1920,
+      cubePositionY: 2
     };
   },
   mounted() {
@@ -85,5 +87,9 @@ $fill-unit: orange;
 
 .right-shade-unit {
   fill: lighten($fill-unit, 30%);
+}
+
+.up-shade-unit {
+  fill: lighten($fill-unit, 40%);
 }
 </style>
